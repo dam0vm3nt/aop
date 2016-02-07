c75d29c50def437895896b11e75138af92dcdf3a
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2016-02-07 01:30:18 +0100

    restyle

--- lib/injector.dart
+++ lib/injector.dart
@@ -26,27 +26,26 @@ class SimpleMethodMatcher implements MethodMatcher {
 
 abstract class PointcutInterceptor implements AstVisitor {
   TextEditTransaction edit;
-
-
 }
 
 class MethodBodyInjector extends RecursiveAstVisitor {
-
   TextEditTransaction edit;
   MethodDeclaration decl;
   String newInvokationContextText;
 
-  MethodBodyInjector(this.newInvokationContextText,this.decl,this.edit);
+  MethodBodyInjector(this.newInvokationContextText, this.decl, this.edit);
 
   visitBlockFunctionBody(BlockFunctionBody node) {
-    logger.fine("Body : \n${node.toSource()}, returns : ${decl.returnType?.name}");
-    if (decl.returnType?.name?.name=="void") {
+    logger.fine(
+        "Body : \n${node.toSource()}, returns : ${decl.returnType?.name}");
+    if (decl.returnType?.name?.name == "void") {
       edit.edit(node.end, node.end, ");}");
-      edit.edit(node.offset, node.offset, "{ ${AopWrappers.AOP_WRAPPER_METHOD_NAME}($newInvokationContextText,() ");
+      edit.edit(node.offset, node.offset,
+          "{ ${AopWrappers.AOP_WRAPPER_METHOD_NAME}($newInvokationContextText,() ");
     } else {
       edit.edit(node.end, node.end, ");}");
-      edit.edit(node.offset, node.offset, "{ return ${AopWrappers.AOP_WRAPPER_METHOD_NAME}($newInvokationContextText,() ");
-
+      edit.edit(node.offset, node.offset,
+          "{ return ${AopWrappers.AOP_WRAPPER_METHOD_NAME}($newInvokationContextText,() ");
     }
     //TextEditTransaction edit = new TextEditTransaction(node.to)
   }
@@ -56,7 +55,7 @@ class AopMixinInjector extends RecursiveAstVisitor {
   List<PointcutInterceptor> interceptors;
   TextEditTransaction edit;
 
-  AopMixinInjector(this.interceptors,this.edit);
+  AopMixinInjector(this.interceptors, this.edit);
 
   visitClassDeclaration(ClassDeclaration classDecl) {
     interceptors.forEach((PointcutInterceptor interceptor) {
@@ -65,69 +64,73 @@ class AopMixinInjector extends RecursiveAstVisitor {
     });
 
     if (edit.hasEdits) {
-      edit.edit(classDecl.name.end,classDecl.name.end," extends Object with AopWrappers");
+      edit.edit(classDecl.name.end, classDecl.name.end,
+          " extends Object with AopWrappers");
     }
-
   }
 }
 
-
-class MethodInterceptorPointcut extends PointcutInterceptor with RecursiveAstVisitor {
+class MethodInterceptorPointcut extends PointcutInterceptor
+    with RecursiveAstVisitor {
   String id;
 
   MethodMatcher matcher;
 
-  MethodInterceptorPointcut({this.id,this.matcher});
+  MethodInterceptorPointcut({this.id, this.matcher});
 
   visitMethodDeclaration(MethodDeclaration node) {
-    logger.fine("Method : ${node.name.toString()} : ${node.offset} - ${node.end}");
+    logger.fine(
+        "Method : ${node.name.toString()} : ${node.offset} - ${node.end}");
     if (matcher.matches(node)) {
       // Recurr on body and inject callbacks to Aop
-      String newInvokationContextText = _createInvokationContextInitializerCode(node);
-      node.accept(new MethodBodyInjector(newInvokationContextText,node,edit));
+      String newInvokationContextText =
+          _createInvokationContextInitializerCode(node);
+      node.accept(new MethodBodyInjector(newInvokationContextText, node, edit));
     }
-
   }
 
   String _createInvokationContextInitializerCode(MethodDeclaration node) {
-    StringBuffer sb = new StringBuffer("new InvokationContext('$id','${node.name}',[");
-    String sep="";
-    node.parameters.parameters.where((FormalParameter fp) => fp.kind!=ParameterKind.NAMED).forEach((FormalParameter fp) {
+    StringBuffer sb =
+        new StringBuffer("new InvokationContext('$id','${node.name}',[");
+    String sep = "";
+    node.parameters.parameters
+        .where((FormalParameter fp) => fp.kind != ParameterKind.NAMED)
+        .forEach((FormalParameter fp) {
       sb.write(sep);
       sb.write(fp.identifier.name);
-      sep=",";
+      sep = ",";
     });
     sb.write("],{");
-    sep="";
-    node.parameters.parameters.where((FormalParameter fp) => fp.kind==ParameterKind.NAMED).forEach((FormalParameter fp) {
+    sep = "";
+    node.parameters.parameters
+        .where((FormalParameter fp) => fp.kind == ParameterKind.NAMED)
+        .forEach((FormalParameter fp) {
       sb.write(sep);
       sb.write("'${fp.identifier.name}':${fp.identifier.name}");
-      sep=",";
+      sep = ",";
     });
     sb.write("})");
     return sb.toString();
   }
-
 }
 
 class InvokationContext {
   String pointcutId;
   String methodName;
   List positionalParameters;
-  Map<String,dynamic> namedParameters;
-  InvokationContext(this.pointcutId,this.methodName,this.positionalParameters,this.namedParameters);
+  Map<String, dynamic> namedParameters;
+  InvokationContext(this.pointcutId, this.methodName, this.positionalParameters,
+      this.namedParameters);
 
   String toString() => "PoincutID:$pointcutId, Method:${methodName}";
 }
 
-
-typedef PointcutHandler(InvokationContext ctx,Function proceed);
+typedef PointcutHandler(InvokationContext ctx, Function proceed);
 
 class PointcutRegistry {
+  Map<String, PointcutHandler> _handlersById = {};
 
-  Map<String,PointcutHandler> _handlersById = {};
-
-  executePointcuts(InvokationContext ctx,Function closure) {
+  executePointcuts(InvokationContext ctx, Function closure) {
     // TODO : replace with a list of pointcutsID
 
     PointcutHandler handler = _handlersById[ctx.pointcutId];
@@ -136,11 +139,10 @@ class PointcutRegistry {
       return closure();
     }
 
-    return handler(ctx,closure);
-
+    return handler(ctx, closure);
   }
 
-  void register(String pointcutId,PointcutHandler handler) {
+  void register(String pointcutId, PointcutHandler handler) {
     _handlersById[pointcutId] = handler;
   }
 
@@ -151,25 +153,24 @@ class PointcutRegistry {
 final PointcutRegistry pointcutRegistry = new PointcutRegistry._();
 
 class AopWrappers {
-  static const String AOP_WRAPPER_METHOD_NAME=r"$aop$";
-  $aop$(InvokationContext context,Function f) {
-    return pointcutRegistry.executePointcuts(context,f);
+  static const String AOP_WRAPPER_METHOD_NAME = r"$aop$";
+  $aop$(InvokationContext context, Function f) {
+    return pointcutRegistry.executePointcuts(context, f);
   }
 }
 
 class Injector {
-
   List<PointcutInterceptor> interceptors;
 
   /**
    * Take a file, analyze it, check the pointcuts and then inject the behavior.
    */
-  Future<String> inject(String contents,String url) async {
+  Future<String> inject(String contents, String url) async {
     CompilationUnit unit = parseCompilationUnit(contents);
-    SourceFile source = new SourceFile(contents, url:url);
-    TextEditTransaction edit = new TextEditTransaction(contents,source);
+    SourceFile source = new SourceFile(contents, url: url);
+    TextEditTransaction edit = new TextEditTransaction(contents, source);
 
-    unit.accept(new AopMixinInjector(interceptors,edit));
+    unit.accept(new AopMixinInjector(interceptors, edit));
 
     if (edit.hasEdits) {
       NestedPrinter p = edit.commit();
--- test/sample.dart
+++ test/sample.dart
@@ -1,12 +1,13 @@
 class Sample {
-  void methodXYZ(String a,{ciccio}) {
+  void methodXYZ(String a, {ciccio}) {
     print(a);
   }
-  int methodA(int b,[int x=10]) {
-    return b+x;
+
+  int methodA(int b, [int x = 10]) {
+    return b + x;
   }
 
-  methodVarRet(x,b,z) {
-    return x+b-z;
+  methodVarRet(x, b, z) {
+    return x + b - z;
   }
 }
